f8bba41b6793be1d5955b3b2f4cf2adf40f35315
Erisdev92 <alejandrogh1992@gmail.com>
2026-03-29 18:29:45 +0000

    landing page react alex

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -7,7 +7,7 @@ const Card = ()=> {
     <div className="card m-3 h-80">
   <img className="card-img-top" src="https://picsum.photos/500/325" alt="Card image cap"/>
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
+    <h5 className="card-title d-flex justify-content-center">Card title</h5>
     <p className="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias dicta minima fugiat doloribus rem debitis, ipsum quas ut tempora. Autem unde laudantium in tempore. Incidunt deleniti aut doloribus laboriosam ut!</p>
     <a href="#" className="btn btn-primary d-flex justify-content-center">Go somewhere</a>
   </div>
